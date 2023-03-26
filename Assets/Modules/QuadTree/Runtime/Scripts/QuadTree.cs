f3c13b895913bcbd334367a1966ac68ba72f613b
Omid Reza Izadi <o.izadi@traviangames.com>
2023-03-26 15:30:45 +0200

    an attempt to use quadtree for collision detection (it works, but the performance is very low)

---
+++ Assets/Modules/QuadTree/Runtime/Scripts/QuadTree.cs
@@ -0,0 +1,122 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+public struct Quadtree
+{
+    private int maxDepth;
+    private int maxObjects;
+    private int depth;
+    private float2 position;
+    private float2 size;
+    private bool nodesCreated;
+
+    public Quadtree(int depth, int maxDepth, int maxObjects, float2 position, float2 size)
+    {
+        this.depth = depth;
+        this.maxDepth = maxDepth;
+        this.maxObjects = maxObjects;
+        this.position = position;
+        this.size = size;
+        nodesCreated = false;
+    }
+
+    public void Insert(NativeArray<Quadtree> nodes, NativeArray<Entity> objects, Entity entity, float2 position)
+    {
+        if (nodesCreated)
+        {
+            int index = GetIndex(position);
+            if (index != -1)
+            {
+                nodes[index].Insert(nodes, objects, entity, position);
+                return;
+            }
+        }
+
+        objects[objects.Length - 1] = entity;
+
+        if (objects[objects.Length - 1] != Entity.Null && depth < maxDepth)
+        {
+            if (!nodesCreated)
+                Split(nodes);
+
+            int i = 0;
+            while (i < objects.Length)
+            {
+                int index = GetIndex(position);
+                if (index != -1)
+                {
+                    nodes[index].Insert(nodes, objects, entity, position);
+                    objects[i] = Entity.Null;
+                }
+                i++;
+            }
+        }
+    }
+
+    public NativeList<Entity> Retrieve(NativeList<Entity> result, NativeArray<Quadtree> nodes, NativeArray<Entity> objects, float2 position)
+    {
+        for (int i = 0; i < objects.Length; i++)
+        {
+            result.Add(objects[i]);
+        }
+
+        if (nodesCreated)
+        {
+            int index = GetIndex(position);
+            if (index != -1)
+            {
+                nodes[index].Retrieve(result, nodes, objects, position);
+            }
+            else
+            {
+                for (int i = 0; i < nodes.Length; i++)
+                {
+                    nodes[i].Retrieve(result, nodes, objects, position);
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private void Split(NativeArray<Quadtree> nodes)
+    {
+        float2 halfSize = size / 2f;
+        nodesCreated = true;
+        nodes[0] = new Quadtree(depth + 1, maxDepth, maxObjects, position + new float2(0, halfSize.y), halfSize);
+        nodes[1] = new Quadtree(depth + 1, maxDepth, maxObjects, position + new float2(halfSize.x, halfSize.y), halfSize);
+        nodes[2] = new Quadtree(depth + 1, maxDepth, maxObjects, position + new float2(halfSize.x, 0), halfSize);
+        nodes[3] = new Quadtree(depth + 1, maxDepth, maxObjects, position, halfSize);
+    }
+
+    private int GetIndex(float2 position)
+    {
+        int result = -1;
+        float2 center = position + size / 2f;
+        bool top = position.y < center.y && position.y + size.y < center.y;
+        bool bottom = position.y > center.y;
+        bool left = position.x < center.x && position.x + size.x < center.x;
+        bool right = position.x > center.x;
+
+        if (top)
+        {
+            if (left)
+                result = 3;
+            else if
+                (right)
+                result = 2;
+        }
+        else if (bottom)
+        {
+            if (left)
+                result = 0;
+            else if (right)
+                result = 1;
+        }
+        return result;
+    }
+}
---
+++ Assets/Modules/QuadTree/Runtime/Scripts/QuadTreeSystem.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Generic;
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+[UpdateBefore(typeof(CollisionSystem))]
+[BurstCompile]
+public class QuadtreeSystem : JobComponentSystem
+{
+    private Quadtree quadtree;
+    private NativeArray<Entity> entities;
+    private NativeArray<Entity> objects;
+    private NativeArray<Quadtree> nodes;
+
+    protected override void OnCreate()
+    {
+        quadtree = new Quadtree(0, 5, 1000, new float2(-100, -100), new float2(200, 200));
+        nodes = new NativeArray<Quadtree>(4, Allocator.Persistent);
+        objects = new NativeArray<Entity>(10000, Allocator.Persistent);
+    }
+
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        var entityQuery = GetEntityQuery(ComponentType.ReadOnly<CollisionComponent>(), ComponentType.ReadOnly<Translation>());
+        entities = entityQuery.ToEntityArray(Allocator.TempJob);
+        var positions = entityQuery.ToComponentDataArray<Translation>(Allocator.TempJob);
+
+        //create a native array of float2 positions from the positions array
+        var positionsArray = new NativeArray<float2>(positions.Length, Allocator.TempJob);
+        for (int i = 0; i < positions.Length; i++)
+        {
+            positionsArray[i] = positions[i].Value.xy;
+        }
+
+        var insertJob = new InsertJob(nodes, objects, entities, positionsArray, quadtree);
+        var insertJobHandle = insertJob.Schedule(entities.Length, 64);
+
+        insertJobHandle.Complete();
+        entities.Dispose();
+        positions.Dispose();
+        positionsArray.Dispose();
+        return JobHandle.CombineDependencies(insertJobHandle, inputDeps);
+    }
+
+    public NativeList<Entity> Retrieve(float2 position)
+    {
+        NativeList<Entity> newEntities = new NativeList<Entity>(Allocator.Temp);
+        quadtree.Retrieve(newEntities, nodes, objects, position);
+        return newEntities;
+    }
+
+    protected override void OnDestroy()
+    {
+        // Dispose of the NativeArray when the system is destroyed
+        nodes.Dispose();
+        objects.Dispose();
+    }
+
+    [BurstCompile]
+    public struct InsertJob : IJobParallelFor
+    {
+        public NativeArray<Entity> entities;
+
+        [NativeDisableParallelForRestriction] //TODO: This is a workaround for the NativeArray<Entity> not being blittable
+        public NativeArray<Entity> objects;
+
+        [NativeDisableParallelForRestriction] //TODO: This is a workaround for the NativeArray<Entity> not being blittable
+        public NativeArray<Quadtree> nodes;
+        public NativeArray<float2> positions;
+        public Quadtree quadtree;
+
+        public void Execute(int index)
+        {
+            quadtree.Insert(nodes, objects, entities[index], positions[index]);
+        }
+
+        public InsertJob(NativeArray<Quadtree> nodes, NativeArray<Entity> objects, NativeArray<Entity> entities, NativeArray<float2> positions, Quadtree quadtree)
+        {
+            this.entities = entities;
+            this.positions = positions;
+            this.quadtree = quadtree;
+            this.nodes = nodes;
+            this.objects = objects;
+        }
+    }
+}
